f72a9dcd783a9ecf1eca99b7b0cb5f202ba84277
MokkeMeguru <meguru.mokke@gmail.com>
2019-03-08 12:28:07 +0900

    fix-file-run_emacs.sh

--- run_emacs.sh
+++ run_emacs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "Please input your ip-addr:port for display (see. your MobaXterm)" 1>&2
+if [ $# -ne 2 ]; then
+    echo -e "Please input \nyour ip-addr:port for display (see. your MobaXterm)\nyour ssh folder (ex. /c/Users/<yourname>/.ssh)" 1>&2
     exit 1
 fi
 
-docker run -e DISPLAY=$1 -v /c/Users/elect/.ssh:/tmp/.ssh -v /:/home/emacs/mnt -it --rm arch-doom-emacs
+docker run -e DISPLAY=$1 -v $2:/tmp/.ssh -v /:/home/emacs/mnt -it --rm arch-doom-emacs
